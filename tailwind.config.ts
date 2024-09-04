494ac7df3621f9745c89ba4179e85ed9c09c6b8e
Agustin Zamar <agustinzamar33@gmail.com>
2024-09-04 08:24:25 -0300

    start project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,14 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
+	theme: {
+		extend: {
+			backgroundImage: {
+				"primary-gradient":
+					"radial-gradient(at top left, rgba(238,174,202,1) 0%, rgba(148,187,233,1) 100%)",
+			},
+		},
+	},
+	plugins: [],
 } satisfies Config;
